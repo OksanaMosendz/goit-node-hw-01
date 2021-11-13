f7f5d0842f9891e57ef9ebe127acabbe03e1fad7
OksanaMosendz <bagheera03@gmail.com>
2021-11-13 02:06:24 +0200

    step 1 done

---
+++ contacts.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const contactsPath = path.join(__dirname, 'contacts.json');
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
